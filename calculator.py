a0f58e8d785189823b1b1a72cde5f3460899243f
YipSP <yipsiewpeng01@gmail.com>
2025-04-19 22:10:35 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -37,7 +37,14 @@ def index():
                     result = add(num1, num2)
                 elif operation == "-":
                     result = subtract(num1, num2)
-
+                elif operation == "x":
+                    result = multiply(num1, num2)
+                elif operation == "÷":
+                    result = divide(num1, num2)
+                else:
+                    result = "Invalid operation"
+                except ValueError:
+                    result = "Please enter valid numbers."
                 
         elif mode == "converter":
             try:
